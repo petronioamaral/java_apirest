9f0eb9f7869f2be006e364b05784d6cf4ce87b74
Petronio Amaral <petronioamaral@gmail.com>
2020-11-17 12:18:00 -0300

    go

---
+++ src/test/java/com/digitalinnovation/restapi/RestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
